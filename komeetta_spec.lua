19196fb7689bfeab929cec85010c8a85dff85a24
Vesa Marttila <vesa.marttila@helsinki.fi>
2010-01-14 01:52:21 +0200

    Added more spec methods.

--- komeetta_spec.lua
+++ komeetta_spec.lua
@@ -6,10 +6,16 @@ List = komeetta.List
 describe["A List"] = function()
     before = function()
         list = List:new()
+        list_with_values = List:new(1, 2, 3, 4)
+        list_from_table = List:new_from_table({ "foo", "bar" })
     end
 
     it["should return 0 length when it is empty"] = function()
         expect(list:length()).should_be(0)
+    end
+
+    it["should return the length of the list"] = function()
+        expect(list_with_values:length()).should_be(4)
     end
 
     it["should return true from null when it is empty"] = function()
@@ -25,9 +31,76 @@ describe["A List"] = function()
             List:new(10, 100, 20))
     end
 
-    it["should not be modified when values are appended"] = function()
-        expect(list:append(100)).should_be(List:new(100))
-        expect(list).should_be(List:new())
+    it['should have a string representation like "List(a b c d)"'] = function()
+        expect(tostring(list)).should_be("List()")
+        expect(tostring(list_with_values)).should_be("List(1 2 3 4)")
+        expect(tostring(list_from_table)).should_be("List(foo bar)")
     end
 
+    it["when cloned should contain the same values"] = function()
+        expect(list_from_table:clone()).should_be(List:new("foo", "bar"))
+    end
+
+    it["should be reversable with the reverse function"] = function()
+        expect(list_from_table:reverse()).should_be(List:new("bar", "foo"))
+    end
+
+    --TODO Test foreach.
+    --it["foreach"] = function()
+    --end
+    
+    it["can be applied with a map function to return a new list"] = function()
+        expect(list_from_table:map(string.upper)).should_be(
+            List:new("FOO", "BAR"))
+    end
+
+    it["can be filtered"] = function()
+        expect(list_with_values:filter(function(n) return n <= 2 end)).
+            should_be(List:new(1, 2))
+    end
+
+    it["can be queried for its head"] = function()
+        expect(list_with_values:head()).should_be(1) 
+    end
+
+    it["can be queried for its tail"] = function()
+        expect(list_with_values:tail()).should_be(List:new(2, 3, 4)) 
+    end
+
+    it["can be concatenated with other lists"] = function()
+        expect(list_with_values:concat(List:new("a", "b"), List:new(
+            "foo", "bar"))).should_be(List:new(1, 2, 3, 4, "a", "b", "foo",
+            "bar")) 
+    end
+
+    it["can be sliced"] = function()
+        expect(list_with_values:slice(2, 3)).should_be(List:new(2, 3)) 
+    end
+
+    it["can be folded from right"] = function()
+        expect(list_with_values:foldr(function(a, b) return a * b end,
+            1)).should_be(24) 
+    end
+
+    it['can be sorted with "qsort"'] = function()
+        expect(List:new(10, -4, 1, -1000):qsort()).should_be(
+            List:new(-1000, -4, 1, 10)) 
+    end
+
+    it['can be sorted with "sorted"'] = function()
+        expect(List:new(10, -4, 1, -1000):sort()).should_be(
+            List:new(-1000, -4, 1, 10)) 
+    end
+
+    it["can be queried for its values from the start of the list"] = function()
+        expect(List:new(10, -4, 1, -1000):take(2)).should_be(List:new(10, -4)) 
+    end
+
+    it["is not mutable by its methods"] = function()
+        expect(list_with_values).should_be(List:new(1, 2, 3, 4))
+        list:reverse():append(100):reverse():append(-21):take(4):map(function(n)
+            return 100 * n end):filter(function(n) return n >= 100 end):concat(
+                List:new(1, 2, -100)):tail():slice(2, 5):sort():take(2)
+        expect(list_with_values).should_be(List:new(1, 2, 3, 4))
+    end
 end
